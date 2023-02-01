5ad7da6486ded417ed78b062d25a9cb2d706037d
Liam Clague <liamclague@Liams-MacBook-Pro.local>
2023-02-01 20:27:42 +0000

    getPasswordOptions function completed

---
+++ script.js
@@ -0,0 +1,135 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+let passwordLength = prompt("Please enter the length of your password (between 8 to 128 characters)");
+if(passwordLength >= 8 && passwordLength <= 128) {
+  let characterType = prompt("Please enter a character type from upper and lower");
+  if(characterType = "upper" || "lower") {
+    let specialCharacter = prompt("Please choose a special character (&%$£ etc)");
+    if(specialCharacters.includes(specialCharacter) == true) {
+      alert("Thank you for your answers!");
+  } else {
+    alert("Character type not recognised.");
+  }
+} else {
+  alert("Password is not the right length.");
+}
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const element = array[i];
+    
+  }
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+getPasswordOptions();
